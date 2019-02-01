fdb4260bcd7afc59a4e5fac2350d4c85270790d9
Pavel Uvarov <p.uvarov@gmail.com>
2019-01-31 21:43:05 -0500

    Add InstaViewController, but not set it up.

---
+++ Project Prototype/InstaViewController.m
@@ -0,0 +1,35 @@
+//
+//  InstaViewController.m
+//  Project Prototype
+//
+//  Created by Pavel on 2019-01-31.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+#import "InstaViewController.h"
+
+@interface InstaViewController ()
+
+@end
+
+@implementation InstaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+  
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
